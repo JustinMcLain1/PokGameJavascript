65ed17a4854672ca3268d5dfadf821c3e99ac128
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-07-24 12:12:50 -0500

    added character to js

--- PokemonGame/index.js
+++ PokemonGame/index.js
@@ -11,6 +11,29 @@ context.fillRect(0,0, canvas.width, canvas.height)
 const image = new Image();
 image.src = './Img/PelletTown.png';
 
+const playerImage = new Image()
+playerImage.src = './Img/playerDown.png'
+
 image.onload = () => {
-	context.drawImage(image, -1100 ,-300);
+	context.drawImage(image, -1070 ,-425);
+	context.drawImage(
+		playerImage,
+		0, 
+		0, 
+		playerImage.width / 4, 
+		playerImage.height,  
+		canvas.width / 2 - (playerImage.width / 4) / 2,  
+		canvas.height / 2 - playerImage.height / 2,
+		playerImage.width / 4, 
+		playerImage.height
+	)
 }
+
+window.addEventListener('keydown',(e) => {
+	switch (e.key){
+		case 'w':
+
+			break
+	}
+})
+
